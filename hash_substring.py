ceda5b04bac3e389531c4c37323c46f8e0d0c3b0
ArtemijsZaharovs <124812244+ArtemijsZaharovs@users.noreply.github.com>
2023-04-15 21:51:11 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,49 @@
 # python3
-
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    iorf = input().rstrip()
+    if iorf == 'F':
+        with open('test_sample.txt', 'r') as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
+    else:
+        pattern = input().rstrip()
+        text = input().rstrip()
+    return pattern, text
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    z = 256
+    q = 101
+    v = len(pattern)
+    n = len(text)
+    o = pow(z, v - 1) % q
+    p = 0
+    t = 0
+    result = []
 
-    # and return an iterable variable
-    return [0]
+    for i in range(v):
+        p = (z * p + ord(pattern[i])) % q
+        t = (z * t + ord(text[i])) % q
 
+    for s in range(n - v + 1):
+        if p == t:
+            if text[s:s + v] == pattern:
+                result.append(s)
+        
+        if s < n - v:
+            t = (z * (t - ord(text[s]) * o) + ord(text[s + v])) % q
 
-# this part launches the functions
+            if t < 0:
+                t += q
+
+    return result
+
+def main():
+    pattern, text = read_input()
+    occurrences = get_occurrences(pattern, text)
+    print_occurrences(occurrences)
+
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
-
+    main()
